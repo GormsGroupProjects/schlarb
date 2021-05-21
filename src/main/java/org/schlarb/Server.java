b05af5eb266a41444f4c35b20dc663a1e74045ed
Gorm <gdsoucie@gmail.com>
2021-05-21 00:17:43 -0400

    added some text to see if jenkins will rebuild

--- src/main/java/org/schlarb/Server.java
+++ src/main/java/org/schlarb/Server.java
@@ -4,6 +4,7 @@ public class Server {
     public static void main(String[] args) {
         //I think we will be using spring for this
         System.out.println("Hello World!");
+        System.out.println("Hello World Again!");
     }
 
 }
