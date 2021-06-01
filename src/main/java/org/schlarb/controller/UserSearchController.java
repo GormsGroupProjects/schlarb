169bb80f5bf1b2c0cc0cf7964876eda91f5387f5
Gorm <gdsoucie@gmail.com>
2021-05-31 22:00:08 -0400

    need to push

--- src/main/java/org/schlarb/controller/UserSearchController.java
+++ src/main/java/org/schlarb/controller/UserSearchController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Set;
 
 @CrossOrigin
 @RestController
@@ -65,10 +64,9 @@ public class UserSearchController {
         else{
             artistService.addArtist(artist);
             user.addArtist(artist);
+            //fixme artists not connected to users
         }
         return user;
-
-
     }
 
 
